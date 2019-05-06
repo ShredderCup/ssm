799ecb502b307d80ffec5bb96bef7c5901052aaa
Shredder Cup <https://github.com/ShredderCup/test1.git>
2019-05-06 15:19:20 +0800

    ssm整合

---
+++ ssm_01/src/main/java/com/czq/pojo/Book.java
@@ -0,0 +1,99 @@
+package com.czq.pojo;
+
+import java.sql.Date;
+
+/**
+ * //                            _ooOoo_
+ * //                           o8888888o
+ * //                           88" . "88
+ * //                           (| -_- |)
+ * //                            O\ = /O
+ * //                        ____/`---'\____
+ * //                      .   ' \\| |// `.
+ * //                       / \\||| : |||// \
+ * //                     / _||||| -:- |||||- \
+ * //                       | | \\\ - /// | |
+ * //                     | \_| ''\---/'' | |
+ * //                      \ .-\__ `-` ___/-. /
+ * //                   ___`. .' /--.--\ `. . __
+ * //                ."" '< `.___\_<|>_/___.' >'"".
+ * //               | | : `- \`.;`\ _ /`;.`/ - ` : | |
+ * //                 \ \ `-. \_ __\ /__ _/ .-` / /
+ * //         ======`-.____`-.___\_____/___.-`____.-'======
+ * //                            `=---='
+ * //
+ * //         .............................................
+ * //                  佛祖镇楼                  BUG辟易
+ * //          佛曰:
+ * //                  写字楼里写字间，写字间里程序员；
+ * //                  程序人员写程序，又拿程序换酒钱。
+ * //                  酒醒只在网上坐，酒醉还来网下眠；
+ * //                  酒醉酒醒日复日，网上网下年复年。
+ * //                  但愿老死电脑间，不愿鞠躬老板前；
+ * //                  奔驰宝马贵者趣，公交自行程序员。
+ * //                  别人笑我忒疯癫，我笑自己命太贱；
+ * //                  不见满街漂亮妹，哪个归得程序员？
+ *
+ * @Description:
+ * @Company:
+ * @Author: czq&wp
+ * @Date: 2019/3/12
+ * @Time: 19:25
+ */
+public class Book {
+
+    private int id;
+    private String title;
+    private String author;
+    private Date publishDate;
+    private String picpath;
+    private double price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(Date publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    public String getPicpath() {
+        return picpath;
+    }
+
+    public void setPicpath(String picpath) {
+        this.picpath = picpath;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
